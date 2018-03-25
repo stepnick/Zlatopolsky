d24d8771df9c7caddb39a525ef54936cc7ce26a5
stepnick <30383932+stepnick@users.noreply.github.com>
2018-03-25 15:35:39 +0300

    Add files via upload

---
+++ 1163.py
@@ -0,0 +1,6 @@
+mas = [1,2,3,-4,5,-6,7,8,9,0]
+k = 0
+for i in range(0, len(mas)):
+    if mas[i] >= 0:
+        k+=1
+print(k)
---
+++ 1215.py
@@ -0,0 +1,9 @@
+import random
+mas = [[1,2,3],[4,5,6],[7,8,9]]
+a = random.choice(random.choice(mas))
+b = random.choice(random.choice(mas))
+print(a + b)
+c = random.choice(random.choice(mas))
+d = random.choice(random.choice(mas))
+e = random.choice(random.choice(mas))
+print((c+d+e)/3)
